ea0621b406c02744d03f1c941e61ea911c0942b8
Kevin Blanchard <blanchardkevinj@gmail.com>
2023-02-18 12:05:03 -0500

    get all linking correct, get sdl2/imgui docking working with examples

---
+++ game/src/main.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <test.hpp>
+
+int main(int , char** )
+{
+    printf("Hello World!, now for demo");
+    talk();
+    demo();
+}
---
+++ goon/src/gnpch.hpp
@@ -0,0 +1,19 @@
+#pragma once
+//Std libs
+#include <utility>
+#include <memory>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <algorithm>
+#include <functional>
+
+//Data Structures
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+//Handle platform specific includes.
+#ifdef GN_PLATFORM_MACOS
+#endif
